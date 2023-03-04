57beacdcaae0d8cd2ff346aadd088859c18898de
w3villa-anjali-jaiswal <124652794+w3villa-anjali-jaiswal@users.noreply.github.com>
2023-03-04 11:38:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,24 +13,35 @@ if (objstr != null) {
 
 display();
 create.onclick = () => {
-    if(edit_id!=null){
-           userarray.splice(edit_id,1,{
+
+
+    if (inputText.value.trim() === "" || inputdate.value === "" || desc.value.trim() === "") {
+        alert("Please fill all the fields");
+      } 
+      else {
+
+
+
+    if (edit_id != null) {
+        userarray.splice(edit_id, 1, {
             taskname: inputText.value,
             date: inputdate.value,
             description: desc.value,
         })
-    }else{
+    } else {
         userarray.push({
             taskname: inputText.value,
             date: inputdate.value,
             description: desc.value,
-        });
+        });   
     }
-    
+      }
     saveinfo(userarray);
     display();
 
 }
+
+
 
 
 function saveinfo(userarray) {
@@ -44,8 +55,11 @@ function display() {
     <td>${user.taskname}</td>
     <td>${user.date}</td>
     <td>${user.description}</td>
-    <td><i class="fa fa-trash-o" style="font-size:24px " onclick='deleteinfo(${i})'></i>
-       <i class="fa fa-edit" style="font-size:24px" onclick='editinfo(${i})'></i>
+    <td>
+    
+    <i class="fa fa-trash-o" style="font-size:24px " onclick='deleteinfo(${i})'></i>    
+    <i class="fa fa-edit" style="font-size:24px" onclick='editinfo(${i})' data-toggle="modal" data-target="#myModal" ></i>
+       
     </td>
   </tr>`
         inputText.value = "";
@@ -55,16 +69,17 @@ function display() {
 
     records.innerHTML = statement;
 }
-function editinfo(id) {
-
-    edit_id = id;
-    inputText.value = userarray[id].taskname;
-    inputdate.value= userarray[id].date;
-    desc.value = userarray[id].description;
-
-}
 function deleteinfo(id) {
     userarray.splice(id, 1)
     saveinfo(userarray);
     display();
 }
+
+function editinfo(id) {
+    edit_id = id;
+    inputText.value = userarray[id].taskname;
+    inputdate.value = userarray[id].date;
+    desc.value = userarray[id].description;
+
+  }
+  
